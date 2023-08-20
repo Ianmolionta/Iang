96d9b9582f1befa56809ed2f465da798eda0100f
Ianmolionta <rian.killer07@gmail.com>
2023-08-20 20:08:41 +0800

    update

--- js/loader.js
+++ js/loader.js
@@ -3,7 +3,8 @@ let id = document.getElementById("loader")
 window.onload = () =>{
     setTimeout(function(){
         id.style.opacity = "0"
-    }, 500);
+        id.style.background = "black"
+    }, 3000);
     setTimeout(function(){
         id.style.display = "none"
     }, 200)
